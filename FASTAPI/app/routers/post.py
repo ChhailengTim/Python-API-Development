f665e52f2abf7b8091271e3d8f0e9ddb50ed481e
CHHAILENG TIM <chhailengtim2018@gmail.com>
2024-03-24 13:21:09 +0700

    add 1h

--- FASTAPI/app/routers/post.py
+++ FASTAPI/app/routers/post.py
@@ -3,6 +3,7 @@ from fastapi import status, HTTPException, Response, Depends, APIRouter
 from sqlalchemy.orm import Session
 import schemas
 import models
+import oauth2
 from database import get_db
 
 router = APIRouter(
@@ -12,7 +13,7 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schemas.Post])
-def get_post(db: Session = Depends(get_db)):
+def get_post(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" SELECT * FROM posts """)
     # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
@@ -20,11 +21,12 @@ def get_post(db: Session = Depends(get_db)):
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES(%s,%s,%s) RETURNING * """, (
     #                post.title, post.content, post.published))
     # new_post = cursor.fetchone()
     # conn.commit()
+    print(current_user)
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
@@ -33,7 +35,7 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
 
 
 @router.get("/{id}", response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db)):
+def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""SELECT * FROM posts WHERE id =  %s""", (str(id),))
     # post = cursor.fetchone()
     post = db.query(models.Post).filter(models.Post.id == id).first()
@@ -44,7 +46,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
+def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute(
     #     """DELETE FROM posts WHERE id = %s returning * """, (str(id),))
     # delete_post = cursor.fetchone()
